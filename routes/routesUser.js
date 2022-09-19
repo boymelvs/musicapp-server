b3297c17df4080e555325140d12a9aa57313b1d0
Melvin <melvin.enmocino@gmail.com>
2022-09-20 00:19:59 +0800

    updates

--- routes/routesUser.js
+++ routes/routesUser.js
@@ -3,10 +3,12 @@ const router = express.Router();
 const controllerLogin = require("../controllers/controllerLogin");
 const controllerSignup = require("../controllers/controllerSignup");
 const controllerEditProfile = require("../controllers/controllerEditProfile");
+const controllertUserDelete = require("../controllers/controllerUserDelete");
 
 router.post("/login", controllerLogin.login);
 router.post("/signup", controllerSignup.signup);
 
 router.post("/profile", controllerEditProfile.update);
+router.delete("/delete/:id", controllertUserDelete.delete);
 
 module.exports = router;
