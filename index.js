24099044cfb34c66f19612a7a38a6cb6ca34f3c2
Melvin <melvin.enmocino@gmail.com>
2022-09-22 09:39:54 +0800

    updates

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const path = require("path");
 const db = require("./database/dbConnect");
+const dotenv = require("dotenv");
 const cors = require("cors");
 const token = require("./controllers/controllerHomepage");
 const morgan = require("morgan");
-const app = express();
 
+dotenv.config({ path: "./.env" });
 const port = process.env.PORT || process.env.SERVER_PORT;
-
+const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
